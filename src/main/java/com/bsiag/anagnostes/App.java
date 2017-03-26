727be174af7b531f9fa6376cf98213ca8eb15b58
Christoph Bräunlich <braeunlich@gmail.com>
2017-03-26 18:40:07 +0200

    changed model to numbers model

--- src/main/java/com/bsiag/anagnostes/App.java
+++ src/main/java/com/bsiag/anagnostes/App.java
@@ -12,7 +12,7 @@ public class App {
 	
 	public static final String DEBUG_PATH = "/home/chb/tmp/";
 	
-	private static final String MODEL_FILE_NAME = "/home/chb/tmp/model.zip";
+	private static final String MODEL_FILE_NAME = "/home/chb/git/anagnostes/src/main/resources/model_numbers.zip";
 	
 	private static final String IMG_PATH = "/home/chb/git/numbers/";
 	
@@ -26,7 +26,7 @@ public class App {
 	private static final String IMG_A_6 = IMG_PATH + "0020_CH4M/6/number-28.png";
 	private static final String IMG_A_7 = IMG_PATH + "0020_CH4M/7/number-20.png";
 	private static final String IMG_A_8 = IMG_PATH + "0020_CH4M/8/number-22.png";
-	private static final String IMG_A_9 = IMG_PATH + "0020_CH4M/9/number-2.png";
+	private static final String IMG_A_9 = IMG_PATH + "0020_CH4M/9/number-6.png";
 	
 	private static final String IMG_B_0 = IMG_PATH + "0021_CH4M/0/number-25.png";
 	private static final String IMG_B_1 = IMG_PATH + "0021_CH4M/1/number-55.png";
@@ -43,12 +43,13 @@ public class App {
 	public static void main(String[] args) throws Exception {
 		LeNetMnist model = new LeNetMnist();
 		
-		model.trainAndSaveNumbersModel(IMG_PATH, new File(MODEL_FILE_NAME));
+//		model.trainAndSaveNumbersModel(IMG_PATH, new File(MODEL_FILE_NAME));
 		
 //		model.trainAndSaveModel(new File(MODEL_FILE_NAME + "2"));
 		
-//		model.loadModel(new File(MODEL_FILE_NAME));
+		model.loadModel(new File(MODEL_FILE_NAME));
 		
+		System.out.println(model.eval(ImageIO.read(new File(IMG_A_0))));
 		System.out.println(model.eval(ImageIO.read(new File(IMG_A_1))));
 		System.out.println(model.eval(ImageIO.read(new File(IMG_A_2))));
 		System.out.println(model.eval(ImageIO.read(new File(IMG_A_3))));
@@ -58,10 +59,11 @@ public class App {
 		System.out.println(model.eval(ImageIO.read(new File(IMG_A_7))));
 		System.out.println(model.eval(ImageIO.read(new File(IMG_A_8))));
 		System.out.println(model.eval(ImageIO.read(new File(IMG_A_9))));
-		System.out.println(model.eval(ImageIO.read(new File(IMG_A_0))));
+		
 		
 		System.out.println("---");
 		
+		System.out.println(model.eval(ImageIO.read(new File(IMG_B_0))));
 		System.out.println(model.eval(ImageIO.read(new File(IMG_B_1))));
 		System.out.println(model.eval(ImageIO.read(new File(IMG_B_2))));
 		System.out.println(model.eval(ImageIO.read(new File(IMG_B_3))));
@@ -71,6 +73,5 @@ public class App {
 		System.out.println(model.eval(ImageIO.read(new File(IMG_B_7))));
 		System.out.println(model.eval(ImageIO.read(new File(IMG_B_8))));
 		System.out.println(model.eval(ImageIO.read(new File(IMG_B_9))));
-		System.out.println(model.eval(ImageIO.read(new File(IMG_B_0))));
 	}
 }
